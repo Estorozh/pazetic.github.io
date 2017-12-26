9ff92d44828cec6fefda5e49deb49c04c09ea691
pazetic <mr.ahilis@gmail.com>
2017-12-27 00:44:05 +0300

    Add files via upload

--- crypto_delta/script.js
+++ crypto_delta/script.js
@@ -6,10 +6,11 @@ function analyz () {
 	finish = +document.getElementsByClassName("finish")[0].value;
 //посчитать процент прибыли
 	let size = +document.getElementsByClassName("size")[0].value;
-	let plus = ((finish-((finish/1000)*2)-start+((start/1000)*2))*size).toFixed(2),
-	procent = ((plus/(start/100))/size).toFixed(2);
-
+	let plus = ((finish-((finish/1000)*2)-start+((start/1000)*2))*size).toFixed(5),
+	procent = ((plus/(start/100))/size).toFixed(5);
 	//покупать или нет
-	let text = "плюс в рублях: <b>"+plus+" рублей </b> <br>плюс в процентах: <b>"+procent+" %</b> ";
+	let text = "покупка: "+(start+((start/1000)*2)).toFixed(5)+"<br>продажа: "+
+	(finish-((finish/1000)*2)).toFixed(5)+"<br>плюс в рублях: <b>"+plus+" рублей</b><br>плюс в процентах: <b>"
+	+procent+" %</b>";
 	document.getElementsByClassName("succesful")[0].innerHTML = text;
 }
